e1db87f1fdb9bcbcec7262a336d34fec43c35f55
mohammadjannaser <mohammadjannaser8@gmail.com>
2022-12-10 23:20:04 +0430

    initial commit

--- app/src/main/java/com/softelino/diceroller/MainActivity.kt
+++ app/src/main/java/com/softelino/diceroller/MainActivity.kt
@@ -5,19 +5,22 @@ import android.os.Bundle
 import android.view.View
 
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-
     private lateinit var textView: TextView
+    private lateinit var imageView: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         textView = findViewById(R.id.text_view)
-        findViewById<Button>(R.id.roll).setOnClickListener { rollDice() }
+        imageView = findViewById(R.id.image_view)
+
+        findViewById<Button>(R.id.roll).setOnClickListener { rollDiceImage() }
     }
 
      private fun rollDice() {
@@ -27,7 +30,22 @@ class MainActivity : AppCompatActivity() {
         textView.visibility = View.VISIBLE
     }
 
+    private fun rollDiceImage() {
 
+        // select the resource based on the selected rolled dice.
+        val resource: Int = when (Dice(6).roll()) {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> throw Exception ("Invalid dice number")
+        }
+        // set the resource image to the image of ui
+        imageView.setImageResource(resource)
+
+    }
 }
 
 
